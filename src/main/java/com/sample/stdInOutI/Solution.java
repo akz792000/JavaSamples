e80d9cba582089f4b7979e088f31da734ab2d088
a.zandi <a.zandi@ocs.ir>
2019-10-28 16:06:19 +0330

    StdIn and StdOut I

---
+++ src/main/java/com/sample/stdInOutI/Solution.java
@@ -0,0 +1,21 @@
+package com.sample.stdInOutI;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            int a = scan.nextInt();
+            // Complete this line
+            // Complete this line
+
+            System.out.println(a);
+            // Complete this line
+            // Complete this line
+        }
+        scan.close();
+    }
+
+}
