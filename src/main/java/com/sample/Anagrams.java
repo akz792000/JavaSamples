e28f8cc766b7297e0befdd8c75d1cd14c2542858
a.zandi <a.zandi@ocs.ir>
2019-11-10 18:33:30 +0330

    Anargam Declaration

--- src/main/java/com/sample/Anagrams.java
+++ src/main/java/com/sample/Anagrams.java
@@ -1,6 +1,10 @@
 package com.sample;
 
 import java.util.Scanner;
+/*
+An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+For example, the word anagram can be rearranged into nag a ram, or the word binary into brainy or the word adobe into abode.
+ */
 
 /*
 --input--
