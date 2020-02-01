9065463906a3c225429f3c5ce8affb9f25bb0077
akz792000 <akz792000@gmail.com>
2020-02-01 09:06:09 +0330

    123

--- src/main/java/com/others/ReplacingBooks.java
+++ src/main/java/com/others/ReplacingBooks.java
@@ -1,9 +1,6 @@
 package com.others;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ReplacingBooks {
 
@@ -45,6 +42,7 @@ public class ReplacingBooks {
 
     class Node {
         Node next;
+        Node prev;
         int start;
         int end;
 
@@ -65,61 +63,117 @@ public class ReplacingBooks {
             return end - start + 1;
         }
 
-        int sum(Node node, int num) {
-            if (node.next == null) {
-                //return weight() + (num > len ? len : num);
-            } else {
-                int diff = node.next.start - node.end - 1;
-                if (num > diff) {
-                    return node.weight() + diff;
-                } else {
-                    return sum(node.next, num - diff);
+        int sum(int remain, boolean calcPrev) {
+            int result = weight(), placement;
+
+            // prev
+            if (calcPrev) {
+                if (remain > 0) {
+                    if (prev == null) {
+                        placement = start;
+                        if (placement >= remain) {
+                            result += remain;
+                            remain -= remain;
+                        } else {
+                            result += placement;
+                            remain -= placement;
+                        }
+                    } else {
+                        placement = start - prev.end - 1;
+                        if (placement >= remain) {
+                            result += remain;
+                            if (placement == remain) {
+                                result += prev.weight();
+                            }
+                            remain -= remain;
+                        } else {
+                            remain -= placement;
+                            result += placement + prev.weight();
+                        }
+                    }
                 }
             }
 
-        }
+            // next
+            if (remain > 0) {
+                if (next == null) {
+                    placement = len - end - 1;
+                    if (placement >= remain) {
+                        result += remain;
+                        remain -= remain;
+                    } else {
+                        result += placement;
+                        remain -= placement;
+                    }
+                } else {
+                    placement = next.start - end - 1;
+                    if (placement >= remain) {
+                        result += remain;
+                        if (placement == remain) {
+                            result += next.weight();
+                        }
+                        remain -= remain;
+                    } else {
+                        remain -= placement;
+                        result += placement + next.sum(remain, false);
+                    }
+                }
+            }
 
+            return result;
+        }
     }
 
     public static int solution(int[] A, int K) {
         len = A.length;
-
-        Map<Integer, Node> map = new HashMap<>();
-
+        int result = 0;
+        Map<Integer, Node> nodes = new HashMap<>();
         for (int i = 0; i < A.length; i++) {
-            Node node = map.get(A[i]);
+            Node node = nodes.get(A[i]);
             if (node == null) {
                 node = new ReplacingBooks().new Node(i, i);
-                map.put(A[i], node);
+                nodes.put(A[i], node);
             } else {
                 node = node.lastNode();
-                if (node.end + 1 == i) {
+                if (i == node.end + 1) {
                     node.end++;
                 } else {
                     node.next = new ReplacingBooks().new Node(i, i);
+                    node.next.prev = node;
                 }
             }
         }
 
-        int result = 0;
-        for (Map.Entry<Integer, Node> entrySet : map.entrySet()) {
+        for (Map.Entry<Integer, Node> entrySet : nodes.entrySet()) {
             Node node = entrySet.getValue();
-            int sum = node.
-            if (result < sum) result = sum;
+            int sum = 0;
+            while (node != null) {
+                sum = node.sum(K, true);
+                if (result < sum) result = sum;
+                node = node.next;
+            }
         }
-
         return result;
     }
 
     public static void main(String[] args) {
-        //System.out.println(solution(new int[]{1}, 0)); // 1
+        System.out.println(solution(new int[]{1, 1, 4, 3, 4, 1, 1}, 2)); // 4
+        System.out.println(solution(new int[]{1}, 0)); // 1
         System.out.println(solution(new int[]{1, 1, 3, 4, 3, 3, 4}, 2)); // 5
-       // System.out.println(solution(new int[]{1, 2, 1, 2, 2, 1, 2}, 0)); // 2
-        //System.out.println(solution(new int[]{1, 2, 1, 2, 2, 1, 2}, 1));  // 4
-        //System.out.println(solution(new int[]{1, 2, 1, 2, 1, 2}, 1));  // 3
-        //System.out.println(solution(new int[]{1, 1, 3, 1}, 2)); // 4
-       // System.out.println(solution(new int[]{4, 5, 5, 4, 2, 2, 4}, 0)); // 2
-       // System.out.println(solution(new int[]{1, 3, 3, 2}, 2)); // 4
+        System.out.println(solution(new int[]{1, 2, 1, 2, 2, 1, 2}, 0)); // 2
+        System.out.println(solution(new int[]{1, 2, 1, 2, 2, 1, 2}, 1));  // 4
+        System.out.println(solution(new int[]{1, 2, 1, 2, 1, 2}, 1));  // 3
+        System.out.println(solution(new int[]{1, 1, 3, 1}, 2)); // 4
+        System.out.println(solution(new int[]{4, 5, 5, 4, 2, 2, 4}, 0)); // 2
+        System.out.println(solution(new int[]{1, 3, 3, 2}, 2)); // 4
+
+        /*
+        int[] samples = new int[Integer.MAX_VALUE];
+        for (int i = 0; i< Integer.MAX_VALUE; i++) {
+            samples[i] = i;
+        }
+        System.out.println(solution(samples, 2)); // 4
+        */
     }
 
 }
