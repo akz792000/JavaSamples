ee5fa91b536af017cea92996d4514e4ee34d5037
a.zandi <a.zandi@ocs.ir>
2020-02-02 18:08:42 +0330

    state

--- src/main/java/com/others/ReplacingBooks.java
+++ src/main/java/com/others/ReplacingBooks.java
@@ -1,11 +1,13 @@
 package com.others;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class ReplacingBooks {
 
-    /*
-    public static int solution(int[] A, int K) {
+    public static int solution0(int[] A, int K) {
         Set<Integer> numbers = new HashSet<>();
         for (int i = 0; i < A.length; i++) numbers.add(A[i]);
         int res = 0;
@@ -36,11 +38,21 @@ public class ReplacingBooks {
         }
         return res;
     }
-    */
 
     static int len = 0;
 
-    class Node {
+    enum Direction {BACKWARD, FORWARD}
+
+    static class Remain {
+        int val;
+
+        Remain(int val) {
+            this.val = val;
+        }
+    }
+
+    static class Node {
+
         Node next;
         Node prev;
         int start;
@@ -63,100 +75,86 @@ public class ReplacingBooks {
             return end - start + 1;
         }
 
-        int sum(int remain, boolean calcPrev) {
+        int sum(Remain remain, Direction direction) {
             int result = weight(), placement;
-
-            // prev
-            if (calcPrev) {
-                if (remain > 0) {
-                    if (prev == null) {
-                        placement = start;
-                        if (placement >= remain) {
-                            result += remain;
-                            remain -= remain;
-                        } else {
-                            result += placement;
-                            remain -= placement;
-                        }
-                    } else {
-                        placement = start - prev.end - 1;
-                        if (placement >= remain) {
-                            result += remain;
-                            if (placement == remain) {
-                                result += prev.weight();
-                            }
-                            remain -= remain;
-                        } else {
-                            remain -= placement;
-                            result += placement + prev.weight();
-                        }
-                    }
-                }
-            }
-
-            // next
-            if (remain > 0) {
-                if (next == null) {
-                    placement = len - end - 1;
-                    if (placement >= remain) {
-                        result += remain;
-                        remain -= remain;
+            Node node = direction.equals(Direction.FORWARD) ? next : prev;
+            if (remain.val > 0) {
+                if (node == null) {
+                    placement = direction.equals(Direction.FORWARD) ? len - end - 1 : start;
+                    if (placement >= remain.val) {
+                        result += remain.val;
+                        remain.val -= remain.val;
                     } else {
                         result += placement;
-                        remain -= placement;
+                        remain.val -= placement;
                     }
                 } else {
-                    placement = next.start - end - 1;
-                    if (placement >= remain) {
-                        result += remain;
-                        if (placement == remain) {
-                            result += next.weight();
+                    placement = direction.equals(Direction.FORWARD) ? node.start - end - 1 : start - node.end - 1;
+                    if (placement >= remain.val) {
+                        result += remain.val;
+                        if (placement == remain.val) {
+                            result += node.weight();
                         }
-                        remain -= remain;
+                        remain.val -= remain.val;
                     } else {
-                        remain -= placement;
-                        result += placement + next.sum(remain, false);
+                        remain.val -= placement;
+                        result += placement + node.sum(remain, direction);
                     }
                 }
             }
-
             return result;
         }
+
+        int sum(int K) {
+            int result = 0;
+            Remain remain;
+            Node node = this;
+            while (node != null) {
+                remain = new Remain(K);
+                int sum = -node.weight();
+                sum += node.sum(remain, Direction.BACKWARD);
+                sum += node.sum(remain, Direction.FORWARD);
+                if (result < sum) result = sum;
+                node = node.next;
+            }
+            return result;
+        }
+
     }
 
     public static int solution(int[] A, int K) {
         len = A.length;
-        int result = 0;
         Map<Integer, Node> nodes = new HashMap<>();
         for (int i = 0; i < A.length; i++) {
             Node node = nodes.get(A[i]);
             if (node == null) {
-                node = new ReplacingBooks().new Node(i, i);
+                node = new Node(i, i);
                 nodes.put(A[i], node);
             } else {
                 node = node.lastNode();
                 if (i == node.end + 1) {
                     node.end++;
                 } else {
-                    node.next = new ReplacingBooks().new Node(i, i);
+                    node.next = new Node(i, i);
                     node.next.prev = node;
                 }
             }
         }
 
+        int result = 0;
         for (Map.Entry<Integer, Node> entrySet : nodes.entrySet()) {
             Node node = entrySet.getValue();
-            int sum = 0;
-            while (node != null) {
-                sum = node.sum(K, true);
-                if (result < sum) result = sum;
-                node = node.next;
-            }
+            int sum = node.sum(K);
+            if (result < sum) result = sum;
         }
         return result;
     }
 
     public static void main(String[] args) {
+
+        //System.out.println(solution0(new int[]{1, 2, 3, 4, 5, 6, 7, 2, 1, 1, 1, 3, 1}, 5)); // 9
+        System.out.println(solution(new int[]{1, 2, 3, 4, 5, 6, 7, 2, 1, 1, 1, 3, 1}, 5)); // 9
+
         System.out.println(solution(new int[]{1, 1, 4, 3, 4, 1, 1}, 2)); // 4
         System.out.println(solution(new int[]{1}, 0)); // 1
         System.out.println(solution(new int[]{1, 1, 3, 4, 3, 3, 4}, 2)); // 5
@@ -167,13 +165,6 @@ public class ReplacingBooks {
         System.out.println(solution(new int[]{4, 5, 5, 4, 2, 2, 4}, 0)); // 2
         System.out.println(solution(new int[]{1, 3, 3, 2}, 2)); // 4
 
-        /*
-        int[] samples = new int[Integer.MAX_VALUE];
-        for (int i = 0; i< Integer.MAX_VALUE; i++) {
-            samples[i] = i;
-        }
-        System.out.println(solution(samples, 2)); // 4
-        */
     }
 
 }
